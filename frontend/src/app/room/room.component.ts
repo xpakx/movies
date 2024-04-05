0ae627c19db1c90a399121b7bf271a674a56b1c0
xpakx <55586470+xpakx@users.noreply.github.com>
2024-04-06 00:35:50 +0200

    feat(frontend): let user open local movie file

--- frontend/src/app/room/room.component.ts
+++ frontend/src/app/room/room.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-room',
@@ -9,9 +9,34 @@ export class RoomComponent implements OnInit {
   name: String = "Room";
   code: String = "code"
 
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  @ViewChild('videoNode') videoNode!: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  openMovieChoice(): void {
+    this.fileInput?.nativeElement.click();
+  }
+
+  loadMovie(event: Event): void {
+    const element = event.currentTarget as HTMLInputElement;
+    let files: FileList | null = element.files;
+    if (!files || files.length < 1) {
+      return;
+    }
+    var file = files[0];
+    var canPlay = this.videoNode.nativeElement.canPlayType(file.type);
+
+    if (canPlay === 'no' || canPlay == '') {
+      return;
+    }
+
+    this.videoNode.nativeElement.src = URL.createObjectURL(file);
+
+  }
+
 
 }
