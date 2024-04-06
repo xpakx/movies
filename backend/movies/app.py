b67517fe0eb7b29c1309e0eca3d75949c88e4049
xpakx <55586470+xpakx@users.noreply.github.com>
2024-04-06 22:42:04 +0200

    feat(backend): create signalling server

--- backend/movies/app.py
+++ backend/movies/app.py
@@ -1,9 +1,11 @@
-from robyn import Robyn
+from robyn import Robyn, ALLOW_CORS, WebSocket
 from robyn.robyn import Request
 from msgspec import json, Struct, ValidationError
 
 app = Robyn(__file__)
+ALLOW_CORS(app, origins=["http://localhost:4200"])
 app.add_response_header("content-type", "application/json")
+websocket = WebSocket(app, "/ws")
 
 
 @app.exception
@@ -27,6 +29,22 @@ async def newRoom(request: Request) -> bytes:
     room: Room = json.decode(request.body, type=Room)
     code = "bxeQrT"
     return json.encode({"name": room.name, "title": room.title, "code": code})
+
+
+@websocket.on("message")
+def message(ws, msg: str) -> str:
+    ws.sync_broadcast(msg)
+    '{"msg": "End of ws."}'
+
+
+@websocket.on("close")
+def close():
+    return '{"msg": "End of ws."}'
+
+
+@websocket.on("connect")
+def connect():
+    return '{"msg": "Connected to ws!"}'
 
 
 if __name__ == "__main__":
