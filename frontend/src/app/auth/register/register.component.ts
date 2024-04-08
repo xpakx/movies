bfcd3ebb840a06809498f7bd3ae569a4f6f16541
xpakx <55586470+xpakx@users.noreply.github.com>
2024-04-08 13:04:50 +0200

    feat(frontend): let user register

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AuthResponse } from '../dto/auth-reponse';
+import { RegisterRequest } from '../dto/register-request';
+import { AuthServiceService } from '../auth-service.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  error: boolean = false;
+  errorMsg: String = "";
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private authService: AuthServiceService) {
+    this.registerForm = this.formBuilder.group({
+      username: [''],
+      password: [''],
+      passwordRe: ['']
+    });
+   }
+
+  ngOnInit(): void {}
+
+  register(): void {
+    console.log(this.registerForm.value);
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    let request: RegisterRequest = {
+      username: this.registerForm.value.username,
+      password: this.registerForm.value.password,
+      passwordRe: this.registerForm.value.passwordRe,
+    };
+
+    console.log(request);
+    this.authService.register(request)
+      .subscribe({
+        next: (response: AuthResponse) => this.onRegister(response),
+        error: (err: HttpErrorResponse) => this.onError(err)
+      });
   }
 
+  onRegister(response: AuthResponse) {
+    this.error = false;
+    localStorage.setItem('token', response.token.toString());
+    localStorage.setItem('username', response.username.toString());
+  }
+
+  onError(err: HttpErrorResponse) {
+    console.log(err);
+    this.error = true;
+    this.errorMsg = err.message;
+  }
 }
